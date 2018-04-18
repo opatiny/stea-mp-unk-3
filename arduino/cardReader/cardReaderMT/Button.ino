b85e6c8bf343fedb1aca1aa0677ff1cd3381cb35
Océane Patiny <oceane@patiny.com>
2018-04-18 21:47:58 +0200

    feat (arduino): add testWakeUp

--- arduino/cardReader/cardReaderMT/Button.ino
+++ arduino/cardReader/cardReaderMT/Button.ino
@@ -6,10 +6,12 @@ NIL_WORKING_AREA(waThreadButton, 0);
 
 NIL_THREAD(ThreadButton, arg) {
 
+ 
+
 
   pinMode(BUTTON_1, INPUT_PULLUP);
   pinMode(BUTTON_2, INPUT_PULLUP);
-
+  unsigned int counter = 0;
   while (true) {
     if (!digitalRead(BUTTON_1)) {
       setParameter(PARAM_MODE, 1); // single song mode
@@ -20,6 +22,21 @@ NIL_THREAD(ThreadButton, arg) {
     }
 
     nilThdSleepMilliseconds(100);
+
+    counter++;
+/*
+    if (getParameter(PARAM_MODE) != 0) {
+      if (counter%20==0) {
+        digitalWrite(7, HIGH);
+      }
+      if (counter%20==10) {
+          digitalWrite(7, LOW);
+      }
+    } else {
+      digitalWrite(7, LOW);
+    }
+    */
+     
   }
 }
 
