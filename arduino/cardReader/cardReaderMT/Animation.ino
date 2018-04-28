44e6923ec562751a714dcf45aab03533ad75458c
Océane Patiny <oceane@patiny.com>
2018-04-28 13:01:24 +0200

    feat (arduino/cardReaderMT): fix rainbow animation

--- arduino/cardReader/cardReaderMT/Animation.ino
+++ arduino/cardReader/cardReaderMT/Animation.ino
@@ -31,6 +31,12 @@ NIL_THREAD(ThreadAnimation, arg) {
     int read2 = analogRead(A5);
     digitalWrite(A0, HIGH);
 
+
+    Serial.print("read1  ");
+    Serial.println(read1);
+    Serial.print("read2  ");
+    Serial.println(read2);
+
     if ((read1 > 100 && read1 < 800) || (read2 > 100 && read2 < 800)) {
       Timer3.attachInterrupt(refresh); // the function attached to timer3 will be executed like an interrupt (delay is defined )
 
@@ -39,7 +45,7 @@ NIL_THREAD(ThreadAnimation, arg) {
         squarePlatinium();
       } else if (read2 > 100 && read2 < 800) {
         setParameter(PARAM_SCAN_ENABLED, -2);
-        animRainbow();
+        animRainbow(2);
       } else {
         setParameter(PARAM_SCAN_ENABLED, 0);
         squareGold();
@@ -78,15 +84,15 @@ void squareGold() {
   off();
 }
 
-void animRainbow() {
+void sowaRainbow() {
   int delayTime = 500;
-  S();
+  S(2);
   nilThdSleepMilliseconds(delayTime);
-  O();
+  O(2);
   nilThdSleepMilliseconds(delayTime);
-  W();
+  W(2);
   nilThdSleepMilliseconds(delayTime);
-  A();
+  A(2);
   nilThdSleepMilliseconds(delayTime);
   off();
   nilThdSleepMilliseconds(delayTime);
@@ -95,40 +101,60 @@ void animRainbow() {
 void squarePlatinium() {
   int delayTime = 250;
   square1(1);
-  delay(delayTime);
+  nilThdSleepMilliseconds(delayTime);
   square2(2);
-  delay(delayTime);
+  nilThdSleepMilliseconds(delayTime);
   square3(1);
-  delay(delayTime);
+  nilThdSleepMilliseconds(delayTime);
   square4(2);
-  delay(delayTime);
+  nilThdSleepMilliseconds(delayTime);
   square5(1);
-  delay(delayTime);
+  nilThdSleepMilliseconds(delayTime);
   square4(2);
-  delay(delayTime);
+  nilThdSleepMilliseconds(delayTime);
   square3(1);
-  delay(delayTime);
+  nilThdSleepMilliseconds(delayTime);
   square2(2);
-  delay(delayTime);
+  nilThdSleepMilliseconds(delayTime);
   square1(1);
-  delay(delayTime);
+  nilThdSleepMilliseconds(delayTime);
   for (byte i = 0; i < 2; i++) {
     squareValue(1);
-    delay(delayTime);
+    nilThdSleepMilliseconds(delayTime);
     squareValue(2);
-    delay(delayTime);
+    nilThdSleepMilliseconds(delayTime);
     squareValue(3);
-    delay(delayTime);
+    nilThdSleepMilliseconds(delayTime);
   }
   for (byte i = 0; i < 10; i++) {
     filedSquare5(1, 2);
-    delay(delayTime / 2);
+    nilThdSleepMilliseconds(delayTime / 2);
     filedSquare5(2, 1);
-    delay(delayTime / 2);
+    nilThdSleepMilliseconds(delayTime / 2);
   }
   off();
 
 }
+
+void animRainbow(byte value) {
+  int delayTime = 100;
+  for (byte row = 0; row < SIZE; row++) {
+    for (byte col = 0; col < SIZE; col++) {
+      matrix[row][col] = value;
+      nilThdSleepMilliseconds(delayTime);
+    }
+  }
+  
+  nilThdSleepMilliseconds(500);
+  
+  for (byte row = SIZE; row > 0; row--) {
+    for (byte col = SIZE; col > 0; col--) {
+      matrix[row-1][col-1] = 0;
+      nilThdSleepMilliseconds(delayTime);
+    }
+  }
+}
+
 
 void squareValue(byte value) {
   for (byte col = 0; col < SIZE; col++) {
@@ -201,45 +227,45 @@ void filedSquare5(byte value1, int value2) {
   }
 }
 
-void S() {
+void S(byte value) {
   off();
   for (byte col = 0; col < SIZE; col++) {
-    matrix[0][col] = true;
-    matrix[SIZE - 3][col] = true;
-    matrix[SIZE - 1][col] = true;
+    matrix[0][col] = value;
+    matrix[SIZE - 3][col] = value;
+    matrix[SIZE - 1][col] = value;
   }
-  matrix[1][4] = true;
-  matrix[3][0] = true;
+  matrix[1][4] = value;
+  matrix[3][0] = value;
 }
 
-void O() {
+void O(byte value) {
   off();
   for (byte col = 0; col < SIZE; col++) {
-    matrix[0][col] = true;
-    matrix[SIZE - 1][col] = true;
-    matrix[col][0] = true;
-    matrix[col][SIZE - 1] = true;
-  }
-}
-
-void W() {
-  off();
-  for (byte col = 0; col < SIZE; col++) {
-    matrix[SIZE - 1][col] = true;
-    matrix[col][0] = true;
-    matrix[col][SIZE - 3] = true;
-    matrix[col][SIZE - 1] = true;
+    matrix[0][col] = value;
+    matrix[SIZE - 1][col] = value;
+    matrix[col][0] = value;
+    matrix[col][SIZE - 1] = value;
   }
 }
 
-
-void A() {
+void W(byte value) {
   off();
   for (byte col = 0; col < SIZE; col++) {
-    matrix[0][col] = true;
-    matrix[SIZE - 3][col] = true;
-    matrix[col][0] = true;
-    matrix[col][SIZE - 1] = true;
+    matrix[SIZE - 1][col] = value;
+    matrix[col][0] = value;
+    matrix[col][SIZE - 3] = value;
+    matrix[col][SIZE - 1] = value;
+  }
+}
+
+
+void A(byte value) {
+  off();
+  for (byte col = 0; col < SIZE; col++) {
+    matrix[0][col] = value;
+    matrix[SIZE - 3][col] = value;
+    matrix[col][0] = value;
+    matrix[col][SIZE - 1] = value;
   }
 }
 
